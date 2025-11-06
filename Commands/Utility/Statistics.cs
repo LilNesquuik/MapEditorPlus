b782af3a717784f98cedb6837a33e6fd293fff79
Morgan Coyaux <zmentionb@gmail.com>
2025-11-06 16:35:24 +0100

    Add statistics command to report admin toy counts

---
+++ Commands/Utility/Statistics.cs
@@ -0,0 +1,33 @@
+﻿using CommandSystem;
+using LabApi.Features.Permissions;
+using LabApi.Features.Wrappers;
+
+namespace ProjectMER.Commands.Utility;
+
+public class Statistics : ICommand
+{
+    /// <inheritdoc/>
+    public string Command => "statistics";
+
+    /// <inheritdoc/>
+    public string[] Aliases { get; } = [ "stats", "sts" ];
+
+    /// <inheritdoc/>
+    public string Description => "Gives statistics about the admintoys spawned.";
+
+    public bool Execute(ArraySegment<string> arguments, ICommandSender sender, out string response)
+    {
+        if (!sender.HasAnyPermission($"mpr.{Command}"))
+        {
+            response = $"You don't have permission to execute this command. Required permission: mpr.{Command}";
+            return false;
+        }
+
+        int adminToyCount = AdminToy.List.Count;
+        int staticToyCount = AdminToy.List.Count(x => x.IsStatic);
+        int diff = adminToyCount - staticToyCount;
+
+        response = $"\nAdmin toys: {adminToyCount}\nStatic toys: {staticToyCount}\nDiff: {diff}";
+        return true;
+    }
+}
